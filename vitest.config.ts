5a93eedb740fb2a3d7d184a9ae84b5c0caa1f510
lucasfelixc <lucasfelixdev@gmail.com>
2025-01-22 18:34:40 -0300

    Wip

--- vitest.config.ts
+++ vitest.config.ts
@@ -21,6 +21,7 @@ export default defineConfig({
         coverage: {
             reporter: ['lcov', 'html'],
             exclude: [
+                'node_modules',
                 './src/tests/**',
             ],
         },
