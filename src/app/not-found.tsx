c6d90e5cdb3d51791fcbaf0d1d9bb047647d3514
lucasfelixc <lucasfelixdev@gmail.com>
2025-11-16 13:36:46 -0300

    Fix not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,15 +1,15 @@
-'use client';
+"use client";
 
-import {useRouter} from 'next/navigation';
-import {Button} from '@/components/ui/Button';
-import {FunctionComponent} from 'react';
-import {WidgetLayout} from '@/layouts/WidgetLayout';
+import { useRouter } from "next/navigation";
+import { Button } from "@/components/ui/Button";
+import { FunctionComponent } from "react";
+import { WidgetLayout } from "@/layouts/WidgetLayout";
 
 const NotFound: FunctionComponent = () => {
     const router = useRouter();
 
     return (
-        <WidgetLayout ongName="404">
+        <WidgetLayout>
             <div className="flex-1 grid w-full place-items-center bg-white px-6 sm:pt-10 sm:pb-14 lg:px-8">
                 <div className="text-center">
                     <p className="text-base font-semibold text-indigo-700">
@@ -19,10 +19,15 @@ const NotFound: FunctionComponent = () => {
                         Não encontramos a página que você procura
                     </h2>
                     <p className="mt-6 text-sm leading-7 text-gray-600">
-                        Certifique-se de estar utilizando um link válido ou tente novamente.
+                        Certifique-se de estar utilizando um link válido ou
+                        tente novamente.
                     </p>
                     <div className="mt-10 flex items-center justify-center gap-x-6">
-                        <Button aria-label="Tentar novamente" variant="brand" onClick={() => router.refresh()}>
+                        <Button
+                            aria-label="Tentar novamente"
+                            variant="brand"
+                            onClick={() => router.refresh()}
+                        >
                             Tentar novamente
                         </Button>
                     </div>
